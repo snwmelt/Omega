73bf9a8e5c3b998e58bd98115408c69f069401d9
Kosetsu Yokai-Suru <ekitai.k@gmail.com>
2015-09-26 15:33:12 +0100

    Added sort code error checking to ExternalAccount

--- Model/ExternalAccount.cs
+++ Model/ExternalAccount.cs
@@ -1,5 +1,6 @@
 ﻿using Omega.Model.Interface;
 using System;
+using System.Text.RegularExpressions;
 
 namespace Omega.Model
 {
@@ -16,7 +17,7 @@ namespace Omega.Model
         /// </summary>
         /// <param name="AccountNumber">Account Number.</param>
         /// <param name="Name">Account Name.</param>
-        /// <param name="SortCode">Account Sort Code: Foramt XX-XX-XX</param>
+        /// <param name="SortCode">Account cort code in the foramt XX-XX-XX or XXXXXX</param>
         public ExternalAccount (UInt32 AccountNumber, String Name, String SortCode)
         {
             this.AccountNumber = AccountNumber;
@@ -43,13 +44,18 @@ namespace Omega.Model
         }
 
         /// <summary>
-        /// Account location (i.e. bank and branch) in the format XX-XX-XX.
+        /// Account location (i.e. bank and branch) in the format XX-XX-XX or XXXXXX.
         /// </summary>
         public String SortCode
         {
             private set
             {
-                _SortCode = value;
+                Regex Rx = new Regex(@"\b([0-9]{2})-?([0-9]{2})-?([0-9]{2})\b");
+
+                if (!Rx.IsMatch(value))
+                    throw new FormatException("Invalid Sort Code.");
+
+                _SortCode = Rx.Replace(value, @"$1-$2-$3");
             }
 
             get
